3f1ff29d5bbcfdcc17ff6a51c370dbec680b0252
Vincent <35411050+avipami@users.noreply.github.com>
2024-05-20 12:06:15 +0200

    Initial Commit

---
+++ HemnetAssignment/HemnetAssignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  HemnetAssignmentApp.swift
+//  HemnetAssignment
+//
+//  Created by Vincent Palma on 2024-05-20.
+//
+
+import SwiftUI
+
+@main
+struct HemnetAssignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
